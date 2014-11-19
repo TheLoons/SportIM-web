c1d9d9bc5b5988ee0f9ef5c8002c1c6b8d69db03
Steven Burnett <sburnett@instructure.com>
2014-11-19 11:43:24 -0700

    Fixed folder stuff we should get better commit messages

--- app/js/home.js
+++ app/js/home.js
@@ -1,6 +1,6 @@
 var home = angular.module('home',['services']);
 
-home.controller('colors', function($scope, User) {
+home.controller('colors', function($scope) {
     $scope.colors = ["blue","red","green"];
 });
 home.controller('users', function($scope, User) {
