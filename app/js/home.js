8f20246d35335f5675d50b1ab6a92d01067ea2dd
Jeff Brock <boccobrock@gmail.com>
2014-12-03 19:54:08 -0700

    added events to feed on home page

--- app/js/home.js
+++ app/js/home.js
@@ -1,8 +1,14 @@
 var home = angular.module('home',['services']);
 
-home.controller('colors', function($scope) {
-    $scope.colors = ["blue","red","green"];
-});
-home.controller('users', function($scope, User) {
-    $scope.users = User.users();
+home.controller('feed', function($scope, Events) {
+    var startDate = moment().format(serviceDateFormat);
+    var endDate = moment().add(3,'M').format(serviceDateFormat);
+    Events.get({start: startDate, end: endDate}).$promise.then(function(resp) {
+        var events = resp.events;
+        angular.forEach(resp.events, function(value, key){
+            events[key].start = moment(value.start).format("MM/DD/YYYY h:mm:ssa");
+            events[key].end = moment(value.end).format("MM/DD/YYYY h:mm:ssa");
+        }); 
+        $scope.events = events;
+      });
 });
