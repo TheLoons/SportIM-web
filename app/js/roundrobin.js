2573f227ad33057a5f65fd531b1c73a420fe8326
Jeff Brock <boccobrock@gmail.com>
2014-12-07 13:23:46 -0700

    fix for foreach on round robin page

--- app/js/roundrobin.js
+++ app/js/roundrobin.js
@@ -4,21 +4,21 @@ calendar.controller('roundrobinc', function($scope, Events, Event) {
     $scope.teamList = [
         {name: "Team 1", id: 1},
         {name: "Team 2", id: 2},
-        {name: "Team 3", id: 2},
-        {name: "Team 4", id: 2},
-        {name: "Team 5", id: 2},
-        {name: "Team 6", id: 2},
-        {name: "Team 7", id: 2},
-        {name: "Team 8", id: 2}
+        {name: "Team 3", id: 3},
+        {name: "Team 4", id: 4},
+        {name: "Team 5", id: 5},
+        {name: "Team 6", id: 6},
+        {name: "Team 7", id: 7},
+        {name: "Team 8", id: 8}
     ];
 
     $scope.teamData = [];
 
     $scope.changeTeam = function () {
-        $scope.teamDate = [];
-        angular.forEach($scope.teamList, function(value,key){
-            if(key.id != $scope.teamSelected.id)
-                $scope.teamDate.push(key);
+        $scope.teamData = [];
+        angular.forEach($scope.teamList, function(value){
+            if(value.id != $scope.teamSelected.id)
+                $scope.teamData.push(value);
         });
     };
 });
