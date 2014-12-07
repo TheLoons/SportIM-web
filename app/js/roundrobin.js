9b7e4ea210ae53bb9874b3b37a95782b4c1dab32
Jeff Brock <boccobrock@gmail.com>
2014-12-07 13:01:09 -0700

    adding more to round robin page

--- app/js/roundrobin.js
+++ app/js/roundrobin.js
@@ -1,13 +1,24 @@
 var calendar = angular.module('roundrobin',['services']);
 
 calendar.controller('roundrobinc', function($scope, Events, Event) {
-    $scope.numGamesSel = [
-        {label: "8 Teams", amount: 8},
-        {label: "4 Teams", amount: 4}
+    $scope.teamList = [
+        {name: "Team 1", id: 1},
+        {name: "Team 2", id: 2},
+        {name: "Team 3", id: 2},
+        {name: "Team 4", id: 2},
+        {name: "Team 5", id: 2},
+        {name: "Team 6", id: 2},
+        {name: "Team 7", id: 2},
+        {name: "Team 8", id: 2}
     ];
 
-    $scope.teamList = [
-        {label: "Team 1", id: 1},
-        {label: "Team 2", id: 2}
-    ];
+    $scope.teamData = [];
+
+    $scope.changeTeam = function () {
+        $scope.teamDate = [];
+        angular.forEach($scope.teamList, function(value,key){
+            if(key.id != $scope.teamSelected.id)
+                $scope.teamDate.push(key);
+        });
+    };
 });
