1166fa6474ce68553d8638d64c908f4aac186233
Jeff Brock <boccobrock@gmail.com>
2014-12-08 20:32:07 -0700

    adding stat tracker from ui-sketch

---
+++ app/js/stattracker.js
@@ -0,0 +1,17 @@
+var calendar = angular.module('stattracker',['services']);
+
+calendar.controller('stattrackerc', function($scope, Events, Event) {
+    $(".player").draggable();
+    $(".soccerball").draggable();
+
+    $(".player").click(function(evt) {
+        var modaltop = $(this).offset().top - $("#player-modal").height() - 20;
+        var modalleft = $(this).offset().left - ($("#player-modal").width() / 3) + 10;
+        $("#player-modal").css({top: modaltop, left: modalleft, display: 'inherit'});
+        evt.stopPropagation();
+    });
+
+    $(document).click(function() {
+        $("#player-modal").css('display', 'none');
+    });
+});
