d3f0187069c5ae179f063a0789befe61f02f8a9b
Jeff Brock <boccobrock@gmail.com>
2015-01-15 14:54:29 -0700

    small fix for login page

--- app/js/login.js
+++ app/js/login.js
@@ -2,5 +2,6 @@ var login = angular.module('login',['services']);
 
 login.controller('login', function($scope, Events) {
 	$scope.loginEvent = function(){
+        console.log($scope.username + " " + $scope.password);
 	};
 });
