958f6028a3f0f19bc04b1dce66bca73b8e5948f4
Jeff Brock <boccobrock@gmail.com>
2015-01-22 17:04:22 -0700

    fix for test

--- karma.conf.js
+++ karma.conf.js
@@ -3,10 +3,11 @@ module.exports = function(config){
 
     files : [
       'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-cookies/angular-cookies.min.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/jquery/dist/jquery.min.js',
+      'app/bower_components/jquery/jquery.min.js',
       'app/bower_components/jquery-ui/ui/minified/jquery-ui.min.js',
       'app/bower_components/fullcalendar/fullcalendar.js',
       'app/bower_components/angular-ui-calendar/src/calendar.js',
