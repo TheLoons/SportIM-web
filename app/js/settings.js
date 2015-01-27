028f61ac328e76aef60e5d2dcf99614b4f1c4d17
Steven Burnett <sburnett@instructure.com>
2015-01-27 15:40:59 -0700

    fixed settings stuff

--- app/js/settings.js
+++ app/js/settings.js
@@ -5,7 +5,12 @@ settings.controller('header', function($scope) {
 
 settings.controller('settingsc', function($scope, User) {
     User.get({email:"stevendburnett@gmail.com"}).$promise.then(function(resp) {
-        var events = resp.user;
-        $scope.events = events.firstName;
+        var user = resp.user;
+        $scope.firstName = user.firstName;
+        $scope.lastName = user.lastName;
+        $scope.phonenumber = user.phone;
+        $scope.email = user.login;
       });
+    $scope.submitEvent = function(){
+    };
 });
