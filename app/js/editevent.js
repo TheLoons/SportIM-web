93e098c4b3112ded8666b85d822b5f220aadf5be
Steven Burnett <sburnett@instructure.com>
2015-02-10 19:37:05 -0700

    added event edit page

--- app/js/editevent.js
+++ app/js/editevent.js
@@ -13,13 +13,31 @@ editevent.controller('editevent', function($scope, $location, UserView, TeamView
     Event.get({id:$location.search().event}).$promise.then(function(resp) {
 
         var event = resp.event;
-        if event.owner != you
         $scope.title = event.title;
+        $scope.startTime = event.start;
+        $scope.endTime = event.end;
+        if (event.teams && event.teams[0] && event.teams[1]) {
+            $('#homeTeam').show()
+            $('#awayTeam').show()
+            $('#homeLabel').show()
+            $('#awayLabel').show()
+            $scope.homeTeam = event.teams[0].id;
+            $scope.awayTeam = event.teams[1].id;
+        }
+        if (!event.editable) {
+            $("#homeTeam").prop("readonly",true);
+            $("#awayTeam").prop("readonly",true);
+            $("#title").prop("readonly",true);
+            $("#description").prop("readonly",true);
+            $("#startTime").prop("readonly",true);
+            $("#endTime").prop("readonly",true);
+            $("#submit").hide();
+        }
       });
+
     UserView.get().$promise.then(function(resp) {
         var people = resp.users;
         $scope.searchUsers = people;
-
         $scope.firstName = people[0].firstName;
 
     });
